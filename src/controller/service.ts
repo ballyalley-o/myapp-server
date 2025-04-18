b8aa331fe0b2513fb77e0ddcd21c4425e90b728d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-18 19:51:41 +1000

    [feat]: enhance error handling in service methods with new error responses and logging

--- src/controller/service.ts
+++ src/controller/service.ts
@@ -1,6 +1,7 @@
 import { Model } from 'mongoose'
 import { Response } from 'express'
 import { GLOBAL } from 'myapp'
+import goodlog from 'good-logs'
 import { User } from 'model'
 import { ErrorResponse } from 'middleware'
 import { CODE, KEY, RESPONSE, Resp } from 'constant'
@@ -33,6 +34,24 @@ export class Service {
     }
 
     res.status(code).cookie(GLOBAL.COOKIE.NAME, token, options).send(Resp.TokenResponse(token, user))
+  }
+
+  public static notFound(): never {
+    throw new ErrorResponse(RESPONSE.ERROR[404], CODE.NOT_FOUND)
+  }
+
+  public static invalid(message: string = RESPONSE.ERROR.INVALID_CREDENTIALS, code: CODE = CODE.UNAUTHORIZED): never {
+    throw new ErrorResponse(message, code)
+  }
+
+  public static alreadyExist(email: string): never {
+    throw new ErrorResponse(RESPONSE.ERROR.EMAIL(email), CODE.CONFLICT)
+  }
+
+  public static catchError(error: any, tag: string, target: string, res: Response) {
+      goodlog.error(error?.message || error?.stack, tag, target)
+      const code = error instanceof ErrorResponse ? error?.code : CODE.BAD_REQUEST
+      res.status(code).send(Resp.Error(error?.message, code, error?.details))
   }
 
   public static async createUser(data: any) {
