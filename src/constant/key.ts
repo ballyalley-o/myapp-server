db51b2ff980aaed1b211ad2f1dcf69f7d174bd4c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-18 19:52:44 +1000

    [constants]: add PASSWORD key to constants for improved error handling

--- src/constant/key.ts
+++ src/constant/key.ts
@@ -1,6 +1,7 @@
 export const KEY = {
   CAST_ERROR         : 'CastError',
   OBJECT_ID          : 'ObjectId',
+  PASSWORD           : 'password',
   PRODUCTION         : 'production',
   PUBLIC             : 'public',
   SCRIPT             : 'script',
