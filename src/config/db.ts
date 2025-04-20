65b4d8856249962f1880526f00a45029b9c3d9c2
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-20 19:09:42 +1000

    myapp [Default] Perf: Trace & Improve performance bottlenecks #14: synchronize user indexes on database connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
 import { GLOBAL } from 'myapp'
 import goodlog from 'good-logs'
+import { User } from 'model'
 
 const TAG = 'DB'
 export const connectDb = async (isConnected: boolean) => {
   try {
     const db = await mongoose.connect(String(GLOBAL.DB_URI))
+    await User.syncIndexes()
     const _host = GLOBAL.DB_HOST(db.connection)
     const _name = GLOBAL.DB_NAME(db.connection)
     goodlog.db(_host, _name, isConnected)
